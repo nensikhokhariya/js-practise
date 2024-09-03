37e8e740a00b0abc2721f4ed26e5c8da4902b8ce
nensikhokhariya <nensipatel13310@gmail.com>
2024-09-03 12:32:05 +0000

    operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -20,8 +20,17 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
  
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //  1 = true ,  0 = false
 //  " " = false
 // "abc" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
---
+++ .devcontainer/01_basics/operations.js
@@ -0,0 +1,31 @@
+// Operations
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+
+
+let str1 = "hello"
+let str2 = " Demo"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+
+let gameCounter = 100
+// gameCounter++  //postfix
+++gameCounter  // prefix
+console.log(gameCounter);
+
+
+
+
