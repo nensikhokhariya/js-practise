667159eb22cc7e66359313bd13794e0f6b1c9f98
nensikhokhariya <nensipatel13310@gmail.com>
2024-09-03 10:25:36 +0000

    conversion operator

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "22abc"
+
+console.log( typeof score);  // first type
+
+// console.log(typeof (score));  // 2nd type
+
+
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// 33 = 33
+//  "33abc" = NaN
+// true = 1;    
+// false = 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ 
+console.log(booleanIsLoggedIn);
+
+//  1 = true ,  0 = false
+//  " " = false
+// "abc" = true
