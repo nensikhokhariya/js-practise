16e971270234d76ac4589a358df0a72962a1d10a
nensikhokhariya <nensipatel13310@gmail.com>
2024-09-09 06:40:19 +0000

    maths

--- .devcontainer/01_basics/Maths.js
+++ .devcontainer/01_basics/Maths.js
@@ -1,1 +1,48 @@
-   
+   console.log(Math);  // math = object
+
+
+//    abs
+console.log('absolute function :');
+console.log(Math.abs(-4));  // negative -> possitive......possitive => possitive
+
+// round
+
+console.log('round function :');
+console.log(Math.round(4.7));
+
+//ceil
+console.log('ceil function :');
+console.log(Math.ceil(4.2));  // ceiling = upper value
+
+// Floor
+console.log('floor function :');
+console.log(Math.floor(4.9));  // floor = lower value
+
+//min max value
+
+console.log('min/max function');
+
+console.log(Math.min(2, 6, 8, 9));
+console.log(Math.max(2, 6, 8, 9));
+
+
+// random function
+
+console.log('random function');
+console.log(Math.random());   //  value always display in 0 or 1
+
+console.log((Math.random()*10) + 1);  
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
